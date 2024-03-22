cb8a2709773d5e7a2df968105c2e477bc1ecfd40
pedro <pedroneves2906@hotmail.com>
2024-03-22 20:25:38 -0300

    Projeto criado

---
+++ Xadrez/tabuleiro/Cor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using tabuleiro;
+using xadrex;
+
+namespace tabuleiro
+{
+    enum Cor
+    {
+        branca,
+        preta,
+        amarela,
+        vermelha,
+        verde,
+        azul,
+    }
+}
---
+++ Xadrez/tabuleiro/TabuleiroException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using tabuleiro;
+using xadrez;
+
+namespace tabuleiro
+{
+  class TabuleiroException : Exception
+    {
+        public TabuleiroException(string msg) : base(msg)
+        {
+        }
+    }
+}
